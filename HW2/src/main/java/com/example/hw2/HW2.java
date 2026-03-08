a8dbf11ccd02b228fab7c167f56d441a8892a325
koooogi <astronaut.kogi@gmail.com>
2026-03-08 23:24:42 +0300

    Add files via upload

---
+++ HW2/src/main/java/com/example/hw2/HW2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.hw2;
+
+/**
+ *
+ * @author kortn
+ */
+public class HW2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
